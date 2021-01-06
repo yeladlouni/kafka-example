349e1bd14f14a8d7f2efd043b81c009b1b1da381
= <=>
2021-01-06 11:24:48 +0100

    Initial commit

---
+++ src/main/java/BasicProducer.java
@@ -0,0 +1,31 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class BasicProducer {
+    public static void main(String[] args) {
+        Properties settings = new Properties();
+        settings.put("client.id", "basic-producer");
+        settings.put("bootstrap.servers", "localhost:9092");
+        settings.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        settings.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        final KafkaProducer<String,String> producer = new KafkaProducer<String, String>(settings);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("### Stopping Basic Producer ###");
+            producer.close();
+        }));
+
+        final String topic = "hello_world_topic";
+
+        for(int i=1; i<=5; i++) {
+            final String key = "key-" + i;
+            final String value = "value-" + i;
+            final ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+            producer.send(record);
+        }
+    }
+}
